7105e4bf8665e6fb8b03f41e1ed3ece5a85f163c
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-07-26 09:10:54 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿global using System.Collections.Generic;
+global using System.Linq;
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+var evenNumbers = numbers.Where(n => n % 2 == 0);
+Console.WriteLine("Even Numbers:");
+foreach (var number in evenNumbers)
+{
+    Console.WriteLine(Convert.ToString(number));
+}
